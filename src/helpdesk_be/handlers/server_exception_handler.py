9f1017f84691cd7cb6fee83a8b4e517f7305c396
hitomi.ozasa <hitomi.ozasa@novel.jpn.com>
2026-08-07 14:48:15 +0900

    fix: handler追記

--- src/helpdesk_be/handlers/server_exception_handler.py
+++ src/helpdesk_be/handlers/server_exception_handler.py
@@ -2,13 +2,20 @@ from fastapi import status
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
 
+from helpdesk_be.core.config.base import core_settings
 from helpdesk_be.schemas.response.v1.error import ErrorResponse
 
 
 def handler(request: Request, exc: Exception) -> JSONResponse:
-    return JSONResponse(
+    response = JSONResponse(
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         content=ErrorResponse(
             detail="システムエラーが発生しました", type="INTERNAL_SERVER_ERROR"
         ).model_dump(),
     )
+    response.headers["Access-Control-Allow-Origin"] = core_settings.front_end_url
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+
+    return response
